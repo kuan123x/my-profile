c9df504570293a639ef8849ba53677960e88bbf6
kuan123x <mathewcarillo143@gmail.com>
2024-09-06 08:38:37 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Mathew Carillo';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'A student who prioritizes having fun and enjoying life over academics. They spend more time with friends and exploring hobbies than studying, focusing on creating memorable experiences rather than striving for top grades.';
+export const ABOUT_TEXT_2 =
+  'Not an excellent student, but a laid-back individual who still cares about maintaining decent grades.';
+export const ABOUT_TEXT_3 =
+  '';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'A 4th-year college student with a basic understanding of Laravel, Vue, and JavaScript.';
+export const RESUME_TEXT_2 =
+  'A 4th-year college student with a basic understanding of Laravel, Vue, and JavaScript.';
